1a77a2875c0d9e90bd77c595ba751af31ef2a42b
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-09-20 22:43:43 +0200

    Fix test for Java platform

--- spec/lib/thor_extensions_spec.rb
+++ spec/lib/thor_extensions_spec.rb
@@ -20,22 +20,34 @@ describe Jenkins::Peace::ThorExtensions do
 
 
   describe '.green' do
-    it { expect(helper.green('foo')).to eq "\e[32mfoo\e[0m" }
+    it 'should render green message' do
+      allow_any_instance_of(Thor::Shell::Color).to receive(:can_display_colors?).and_return(true)
+      expect(helper.green('foo')).to eq "\e[32mfoo\e[0m"
+    end
   end
 
 
   describe '.red' do
-    it { expect(helper.red('bar')).to eq "\e[31mbar\e[0m" }
+    it 'should render red message' do
+      allow_any_instance_of(Thor::Shell::Color).to receive(:can_display_colors?).and_return(true)
+      expect(helper.red('bar')).to eq "\e[31mbar\e[0m"
+    end
   end
 
 
   describe '.bold' do
-    it { expect(helper.bold('boo')).to eq "\e[1mboo\e[0m" }
+    it 'should render bold message' do
+      allow_any_instance_of(Thor::Shell::Color).to receive(:can_display_colors?).and_return(true)
+      expect(helper.bold('boo')).to eq "\e[1mboo\e[0m"
+    end
   end
 
 
   describe '.formated_headers' do
-    it { expect(helper.formated_headers).to eq ["\e[1mVersion\e[0m", "\e[1mLocation\e[0m", "\e[1mClasspath\e[0m", "\e[1mInstalled\e[0m"] }
+    it 'should render colored headers' do
+      allow_any_instance_of(Thor::Shell::Color).to receive(:can_display_colors?).and_return(true)
+      expect(helper.formated_headers).to eq ["\e[1mVersion\e[0m", "\e[1mLocation\e[0m", "\e[1mClasspath\e[0m", "\e[1mInstalled\e[0m"]
+    end
   end
 
 
@@ -79,6 +91,7 @@ describe Jenkins::Peace::ThorExtensions do
 
     context 'when wars file are installed' do
       it 'should return a formated war files list (an array of array)' do
+        allow_any_instance_of(Thor::Shell::Color).to receive(:can_display_colors?).and_return(true)
         expect(Jenkins::Peace).to receive(:list).and_return([Jenkins::Peace.build_war_file('1.629')])
         expect(helper.formated_war_files_list).to eq VALID_LIST
       end
